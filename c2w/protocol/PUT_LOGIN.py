16e94a2de2cfe7e82b74d81bd36357171269199e
Quoc Anh TRINH <qtrinh@pc-iti-193.priv.enst-bretagne.fr>
2016-11-25 15:25:09 +0100

    put_login and response_login

--- c2w/protocol/PUT_LOGIN.py
+++ c2w/protocol/PUT_LOGIN.py
@@ -3,9 +3,12 @@ from twisted.internet.protocol import DatagramProtocol
 import struct
 import time 
 
-def PUT_LOGIN(userName):
-    l = str(len(userName))
-    code = '1s' + str(len(userName)) + 's'
-    data = struct.pack(code,l.encode('utf-8'),userName.encode('utf-8'))
-    print(data)
+def PUT_LOGIN(seq_number,username):
+
+    message_type = 0;
+    user_id = 0;
+    message_length = 1 + len(username)
+    UL = len(username)
+    code = '!BHBH' + 'B' + str(message_length) + 's'
+    data = struct.pack(code,message_type,seq_number,user_id,message_length,UL,username.encode('utf-8'));
     return data
