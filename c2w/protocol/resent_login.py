9e6ed2e79b1744a84f784ec9d0c5e29a6e72d8eb
Quoc Anh TRINH <qtrinh@pc-df-201.priv.enst-bretagne.fr>
2016-11-28 21:59:18 +0100

    we forgot saving 1 file

--- c2w/protocol/resent_login.py
+++ c2w/protocol/resent_login.py
@@ -4,7 +4,7 @@ def resent_login(seq_number,username,successful_login,transport,packet,serverAdd
 
     if successful_login == 0: # packet lost
         transport.write(packet,(serverAddress,serverPort))
-        reactor.callLater(delay, resent_login.resent_login,self.seq_number,username,successful_login,transport,packet,serverAddress,serverPort,delay)
+        reactor.callLater(delay, resent_login.resent_login,seq_number,username,successful_login,transport,packet,serverAddress,serverPort,delay)
         print(1)
         
 
